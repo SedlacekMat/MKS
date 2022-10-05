47668ab191cf2c9e837962e09ddc04d443ccfa67
xsedla1m@vutbr.cz <xsedla1m@vutbr.cz>
2022-10-05 15:47:27 +0200

    CV02 ukol4 kompleto

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -20,21 +20,91 @@
 #include "stm32f0xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
+
+volatile uint32_t Tick;
+
+//Symbol defs
+#define LED_TIME_BLINK 300
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
+
+void EXTI0_1_IRQHandler(void);
+void SysTick_Handler(void);
+void blikac(void);
+void tlacitka(void);
 
 int main(void)
 {
-	static const uint8_t pole[32] = {1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,0,0,0,0,0,0};
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
+	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; //hodziny
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	//NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+
+	SysTick_Config(8000); // timer 1ms
+
 	/* Loop forever */
 	for(;;){
-		for (uint8_t i = 0; i < sizeof(pole); i++) {
-			if (pole[i]) GPIOA->BSRR = (1<<5); // set
-			else GPIOA->BRR = (1<<5); // reset
-			for (volatile uint32_t i = 0; i < 100000; i++) {}
-		}
+		tlacitka();
 	}
 }
+
+void EXTI0_1_IRQHandler(void)
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1<<0);
+	}
+}
+
+void SysTick_Handler(void)
+{
+	Tick++;
+}
+
+void blikac(void)
+{
+	static uint32_t delay;
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1<<4);
+		delay = Tick;
+	}
+}
+
+void tlacitka(void)
+{
+	static uint32_t old_s2;
+	static uint32_t old_s1;
+	static uint32_t off_time;
+	uint32_t new_s2 = GPIOC->IDR & (1<<0);
+	uint32_t new_s1 = GPIOC->IDR & (1<<1);
+
+	if (old_s2 && !new_s2) { // falling edge
+	off_time = Tick + LED_TIME_SHORT;
+	GPIOB->BSRR = (1<<0);
+	}
+	old_s2 = new_s2;
+
+	if (old_s1 && !new_s1) { // falling edge
+	off_time = Tick + LED_TIME_LONG;
+	GPIOA->BSRR = (1<<4);
+	}
+	old_s1 = new_s1;
+
+	if (Tick > off_time) {
+	GPIOB->BRR = (1<<0);
+	GPIOA->BRR = (1<<4);
+	}
+}
+
+
+
+
