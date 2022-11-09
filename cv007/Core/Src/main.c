cb2a06bb8c94d449c5723db27a4473beab9e479b
xsedla1m@vutbr.cz <xsedla1m@vutbr.cz>
2022-11-09 15:15:45 +0100

    done

--- cv007/Core/Src/main.c
+++ cv007/Core/Src/main.c
@@ -22,7 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lis2dw12_reg.h"
+#include "stdio.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -62,12 +63,33 @@ void StartVisualTask(void const * argument);
 void StartAcceleroTask(void const * argument);
 
 /* USER CODE BEGIN PFP */
-
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp, uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp, uint16_t len);
+static stmdev_ctx_t lis2dw12 = {
+		.write_reg = platform_write,
+		.read_reg = platform_read,
+		.handle = &hi2c1,
+};
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int _write(int file, char const *buf, int n)
+{
+	/* stdout redirection to UART2 */
+	HAL_UART_Transmit(&huart2, (uint8_t*)(buf), n, HAL_MAX_DELAY);
+	return n;
+}
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp, uint16_t len)
+{
+	HAL_I2C_Mem_Write(handle, LIS2DW12_I2C_ADD_H, reg, I2C_MEMADD_SIZE_8BIT, (uint8_t*)bufp, len, 1000);
+	return 0;
+}
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp, uint16_t len)
+{
+	HAL_I2C_Mem_Read(handle, LIS2DW12_I2C_ADD_H, reg, I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+	return 0;
+}
 /* USER CODE END 0 */
 
 /**
@@ -345,7 +367,8 @@ void StartDefaultTask(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-	  	  int16_t msg;
+	  osDelay(1);
+	  	 /* int16_t msg;
 
 	  	  xQueueSend(xVisualQueueHandle, &msg, 0);
 
@@ -363,7 +386,7 @@ void StartDefaultTask(void const * argument)
 
 	  	  msg = 0;
 	  	  xQueueSend(xVisualQueueHandle, &msg, 0);
-	  	  osDelay(300);
+	  	  osDelay(300);*/
   }
   /* USER CODE END 5 */
 }
@@ -413,11 +436,35 @@ void StartVisualTask(void const * argument)
 /* USER CODE END Header_StartAcceleroTask */
 void StartAcceleroTask(void const * argument)
 {
-  /* USER CODE BEGIN StartAcceleroTask */
-  /* Infinite loop */
+	/* USER CODE BEGIN StartAcceleroTask */
+	lis2dw12_full_scale_set(&lis2dw12, LIS2DW12_2g);
+	lis2dw12_power_mode_set(&lis2dw12, LIS2DW12_CONT_LOW_PWR_LOW_NOISE_2);
+	lis2dw12_block_data_update_set(&lis2dw12, PROPERTY_ENABLE);
+	lis2dw12_fifo_mode_set(&lis2dw12, LIS2DW12_STREAM_MODE); // enable continuous FIFO
+	lis2dw12_data_rate_set(&lis2dw12, LIS2DW12_XL_ODR_25Hz); // enable part from power-down
+	// Check device ID
+	uint8_t whoamI = 0;
+	lis2dw12_device_id_get(&lis2dw12, &whoamI);
+	printf("LIS2DW12_ID %s\n", (whoamI == LIS2DW12_ID) ? "OK" : "FAIL");
+	/* Infinite loop */
   for(;;)
   {
-	  osDelay(1);
+	  uint8_t samples;
+	  int16_t raw_acceleration[3];
+	  lis2dw12_fifo_data_level_get(&lis2dw12, &samples);
+	  /*for (uint8_t i = 0; i < samples; i++) {
+		  // Read acceleration data
+		  lis2dw12_acceleration_raw_get(&lis2dw12, raw_acceleration);
+		  printf("X=%d Y=%d Z=%d\n", raw_acceleration[0], raw_acceleration[1], raw_acceleration[2]);
+		  osDelay(1000);
+	  }*/
+	  for (uint8_t i = 0; i < samples; i++) {
+	  		  // Read acceleration data
+	  		  lis2dw12_acceleration_raw_get(&lis2dw12, raw_acceleration);
+	  		  // printf("X=%d Y=%d Z=%d\n", raw_acceleration[0], raw_acceleration[1], raw_acceleration[2]);
+	  	  }
+	  xQueueSend(xVisualQueueHandle, &raw_acceleration[0], 0);
+	  osDelay(50);
   }
   /* USER CODE END StartAcceleroTask */
 }
